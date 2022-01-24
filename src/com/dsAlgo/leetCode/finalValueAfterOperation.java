c8a8b8d62eefe5888b5b70e8da499656adaebabd
nayankamalverma <nayankamalverma@gmail.com>
2022-01-24 23:15:19 +0530

    init

--- src/com/dsAlgo/leetCode/finalValueAfterOperation.java
+++ src/com/dsAlgo/leetCode/finalValueAfterOperation.java
@@ -1,18 +1,18 @@
 package com.dsAlgo.leetCode;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class finalValueAfterOperation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n= sc.nextInt();
+        int n = sc.nextInt();
         String arr[] = new String[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.next();
         }
         System.out.println(finalValue(arr));
     }
-
     static int finalValue(String[] arr) {
         int x=0;
         for (String i: arr) {
