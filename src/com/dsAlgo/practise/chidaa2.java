fcb87a23e99e330f52271b510022539719f58dd1
Nayan <nayankamalverma@gmail.com>
2022-06-15 00:30:34 +0530

    init

---
+++ src/com/dsAlgo/practise/chidaa2.java
@@ -0,0 +1,27 @@
+package com.dsAlgo.practise;
+
+public class chidaa2
+{
+    public static void main(String[] args) {
+//            int result =0;
+//            Short s= 42;
+//            Long x = new Long("42");
+//            Long y = new Long(42);
+//        Short z = new Short("42");
+//        Short x2 = new Short(s);
+//        Integer y2= new Integer("42");
+//        Integer z2 = new Integer(42);
+//
+//        if (x==y)
+//            result=1;
+//        if (x.equals(y))
+//            result+=10;
+//        if (x.equals(z))
+//            result+=100;
+//        if (x.equals(x2))
+//            result+=1000;
+//        if (x.equals(z2))
+//            result+=10000;
+//        System.out.println(result);
+    }
+}
