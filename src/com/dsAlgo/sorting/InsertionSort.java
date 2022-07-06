96fbaf5bb6beeb793517aa50a487fcf862d86605
Nayan <nayankamalverma@gmail.com>
2022-07-07 02:57:29 +0530

    init

--- src/com/dsAlgo/sorting/InsertionSort.java
+++ src/com/dsAlgo/sorting/InsertionSort.java
@@ -4,12 +4,26 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int[] arr= new int[]{5,2,4,3,1};
+        int[] arr= new int[]{2,1,5,4,1,3};
         insertionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     private static void insertionSort(int[] arr) {
-
+        for (int i = 0; i < arr.length-1 ; i++) {
+            for (int j = i+1; j >0 ; j--) {
+                if(arr[j-1]<arr[j]){
+                    break;
+                }else{
+                    int temp =arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+            }
+        }
     }
+
+//    private static void swap(int[] arr,int i,int j){
+//
+//    }
 }
